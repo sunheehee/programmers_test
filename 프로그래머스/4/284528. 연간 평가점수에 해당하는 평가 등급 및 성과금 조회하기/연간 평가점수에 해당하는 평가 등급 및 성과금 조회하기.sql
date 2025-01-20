9c185f301b6de5c46bd77f37b978382b6ee1f5c6
sunheehee <poohit703@naver.com>
2025-01-20 14:09:37 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,29 @@
+-- 코드를 작성해주세요
+SELECT
+    E.EMP_NO,
+    E.EMP_NAME,
+    (CASE 
+        WHEN AVG(G.SCORE) >= 96 THEN 'S'
+        WHEN AVG(G.SCORE) >= 90 THEN 'A'
+        WHEN AVG(G.SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+     END) AS GRADE,
+    (CASE 
+        WHEN AVG(G.SCORE) >= 96 THEN E.SAL * 0.2
+        WHEN AVG(G.SCORE) >= 90 THEN E.SAL * 0.15
+        WHEN AVG(G.SCORE) >= 80 THEN E.SAL * 0.1
+        ELSE 0
+     END ) AS BONUS
+    
+FROM HR_EMPLOYEES AS E
+JOIN HR_GRADE AS G 
+ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY E.EMP_NO;
+
+# GROUP BY E.EMP_NO --- AVG를 사용했으니까 GROUPING해줘야함
+# CASE WHEN 사용법 숙지하기
+
+    
+
+    
