7e43928d47113e10c1acf38d81b90786c0e1c44f
Sunhee Jang <poohit703@naver.com>
2025-07-20 01:00:27 +0900

    Update 피자 나눠 먹기 （2）.py

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,5 +1,14 @@
+# math모듈 활용
 import math
 def solution(n):
     gcd = math.gcd(6,n)
     lcm = (6 * n) / gcd
     return lcm / 6
+
+# math 모듈 활용 x
+def solution(n):
+    i = 1
+    while True:
+        if 6 * i % n == 0:
+            return i
+        i += 1  
