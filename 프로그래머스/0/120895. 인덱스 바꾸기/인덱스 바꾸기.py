e8cd9b68bfad93f24fb9d2be4d7dca28ca9c9c8e
Sunhee Jang <poohit703@naver.com>
2025-08-13 23:20:16 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,6 +1,4 @@
 def solution(my_string, num1, num2):
-    s = []
-    for string in my_string:
-        s.append(string)
+    s = list(my_string)
     s[num1], s[num2] = s[num2], s[num1]
     return ''.join(s)
